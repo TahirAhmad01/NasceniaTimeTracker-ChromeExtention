1aa8fe2cd6bd7dbdd712d5f5c78c4059f68fb25e
Tahir Ahmad <tahirahmad0234@gmail.com>
2023-12-23 12:42:28 +0600

    code clean

--- popup.js
+++
@@ -1,6 +0,0 @@
-
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.action === "calculateAttendanceTime") {
-    document.getElementById("result").innerText = message.result;
-  }
-});
